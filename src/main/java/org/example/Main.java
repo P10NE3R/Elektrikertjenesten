1ee8960f976f1c4a68a8fd2b2bc6a95cd147f955
H.S <henrik1708@gmail.com>
2026-06-04 10:21:26 +0200

    Int

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,8 +16,6 @@ public class Main {
     public static void main(String[] args) {
         AzureClient azure = new AzureClient();
         azure.AzureTester();
-
-        ContractingWorksClient cw = new ContractingWorksClient();
         
 
         AzureClient.closeCache();
